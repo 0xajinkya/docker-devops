1b33f93cd1ddf8ec3319b301427072bc344fa064
Ajinkya Kahane <kahaneajinkya051@gmail.com>
2023-09-27 23:03:04 +0530

    add: connect to db

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://ajinkya:ajinkya@172.18.0.2:27017/?authSource=admin")
+        .then(() => {
+            console.log("Successfully connected to database!")
+        })
+        .catch((err) => {
+            console.log(err)
+        });
 
 app.get("/", (req, res) => {
     res.send("Hi, there");
